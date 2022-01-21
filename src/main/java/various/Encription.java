8cb769b9a3598b8c18726bc6a47825bfa07c3ead
Dan Ionescu <dan.ionescu@inovo.ro>
2022-01-21 18:47:37 +0200

    added problem named encription

---
+++ src/main/java/various/Encription.java
@@ -0,0 +1,2 @@
+package various;public class Encription {
+}
---
+++ src/test/java/various/EncriptionTest.java
@@ -0,0 +1,11 @@
+package various;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class EncriptionTest {
+
+    @Test
+    void encryption() {
+    }
+}
